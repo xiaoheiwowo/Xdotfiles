6a6ea86e2455bd96b65601a85f30fad69bbdcea8
pengyu.liu <pengyu.liu@ewp-group.com>
2025-02-27 10:36:18 +0800

    fish config

--- fish/config.fish
+++ fish/config.fish
@@ -14,6 +14,13 @@ alias yy=yazi
 alias where=which
 alias cat="bat --style=header --paging=never --theme=gruvbox-dark"
 alias kj="kubectl exec -it appserver-jenkins-inbound-agent-001 -n devops -- /bin/fish"
+alias code=cursor
+alias dev="ssh bytepower-server-stage.dev.cn"
+alias k="kubectl"
+alias kc="kubectl config use-context"
+alias kas="kubectl -n appserver"
+alias kff="kubectl -n appserver-045"
+alias kbp="kubectl -n bytepower"
 
 set fish_greeting ""
 # set -Ux LC_ALL "zh_CN.UTF-8"
